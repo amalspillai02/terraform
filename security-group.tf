1122b72fcabb3866dce1922a999e8575074248eb
amalspillai02 <amalspillai2002@gmail.com>
2025-04-04 10:09:31 +0530

    added tf file for creating security group in aws

---
+++ security-group.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "ap-south-1"  # Change this to your preferred region
+}
+
+resource "aws_security_group" "example_sg" {
+  name        = "terraform-sg"
+  description = "Example security group with inbound and outbound rules"
+
+  # Inbound rules
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allows SSH from anywhere (change this for security)
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allows HTTP from anywhere
+  }
+
+  # Outbound rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Allows all outbound traffic
+  }
+
+  # Tags
+  tags = {
+    Name        = "terraform-security-group"
+  }
+}
