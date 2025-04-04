f89c7e4b36bea9eafc5f5ba125cc99c281b07d0a
amalspillai02 <amalspillai2002@gmail.com>
2025-04-04 10:42:03 +0530

    added tf code for creating an instance

---
+++ ec-instance.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-south-1"  # Change this to your preferred region
+}
+resource "aws_instance" "example_instance" {
+  ami           = "ami-002f6e91abff6eb96"  # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+  key_name      = "main-machine-key"  # Replace with your key pair name
+
+  security_groups = [aws_security_group.example_sg.name]
+
+  tags = {
+    Name        = "example-instance"
+  }
+}
