13c43d034bf6b8ab33be7aec6aaf98e764be1a16
amalspillai02 <amalspillai2002@gmail.com>
2025-04-05 10:27:38 +0530

    commented

--- s3-creation.tf
+++ s3-creation.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "amalspilli-tf-bucket" # must be globally unique
-  acl    = "private"
+# resource "aws_s3_bucket" "my_bucket" {
+#  bucket = "amalspilli-tf-bucket" # must be globally unique
+#  acl    = "private"
 
-  tags = {
-    Name        = "MyBucket"
-  }
-}
+# tags = {
+#    Name        = "MyBucket"
+#  }
+#}
 
